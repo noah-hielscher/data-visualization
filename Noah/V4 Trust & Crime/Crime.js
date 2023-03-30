0e9c452f34b3c7c875a85f309d4fe46660b461f3
Noah Hielscher <noah.hielscher@hfg.design>
2023-03-30 19:35:34 +0200

    V4 Datenbank Vereinigung

---
+++ Noah/V4 Trust & Crime/Crime.js
@@ -0,0 +1,41 @@
+/*
+Marsha, Noah, Lukas
+*/
+
+class Country {
+	constructor() {
+		//Datenbank
+		this.myIso = "NaN";
+		this.myCountry = "NaN";
+		this.myRegion = "NaN";
+		this.mySubregion = "NaN";
+		this.myCategory = "NaN";
+		this.mySex = "NaN";
+		this.myAge = "NaN";
+		this.myYear = "NaN";
+		this.myValue = "NaN";
+		//Sonstiges
+		this.mySize = 0;
+		this.myWidth = 6;
+		this.myColor = color(100, 100, 255);
+		this.estaEncima = false;
+	}
+
+	display(myX, myY) {
+		this.estaEncima =
+			mouseX > myX &&
+			mouseX < myX + this.myWidth &&
+			mouseY > myY - this.mySize &&
+			mouseY < myY;
+
+		noStroke();
+		fill(this.myColor);
+		rect(myX, myY, this.myWidth, -this.mySize);
+
+		if (this.estaEncima) {
+			fill(200);
+			text(this.myCountryArea, myX, myY + 30);
+			text(this.myCountry, myX, myY + 15);
+		}
+	} // end of display
+} // end of class
---
+++ Noah/V4 Trust & Crime/sketch.js
@@ -0,0 +1,104 @@
+/*
+Marsha, Noah, Lukas
+*/
+
+//Datenbank Crime
+let crimeData;
+let myCrime = [];
+
+//Datenbank Trust
+let trustData;
+let myTrust = [];
+
+function preload() {
+	trustData = loadTable("data/trust.csv", "csv", "header");
+	crimeData = loadTable("data/Crime.csv", "csv", "header");
+}
+
+function setup() {
+	createCanvas(1400, 800);
+
+	//Import Crime
+	let c = 0;
+
+	for (let myRow of crimeData.rows) {
+		let currentCountry = new Country();
+
+		//Import
+		currentCountry.myIso = myRow.get("Iso3_code");
+		currentCountry.myCountry = myRow.get("Country");
+		currentCountry.myRegion = myRow.get("Region");
+		currentCountry.mySubregion = myRow.get("Subregion");
+		currentCountry.myCategory = myRow.get("Category");
+		currentCountry.mySex = myRow.get("Sex");
+		currentCountry.myAge = myRow.get("Age");
+		currentCountry.myYear = myRow.get("Year");
+		currentCountry.myValue = myRow.get("VALUE");
+
+		//Map
+		currentCountry.mySize = map(
+			currentCountry.myCountryArea,
+			17098250,
+			50,
+			700,
+			5
+		); // [17098250,50]
+
+		currentCountry.myColor = color(200, 100, 100);
+
+		myCrime[c] = currentCountry;
+		c++;
+	}
+
+	//Import Trust
+
+	let i = 0;
+
+	for (let myRow of trustData.rows) {
+		let currentTrust = new TrustinPolice();
+
+		//ImportDatenbank
+		currentTrust.myCountry = myRow.get("Countries");
+		currentTrust.myYears = myRow.get("Periods");
+		currentTrust.myTrustinPoliceC = myRow.get(
+			"Evaluation of trust in/Police (score)"
+		);
+		currentTrust.myTrustinPoliceP = myRow.get(
+			"Percentage of people with trust in/Police (%)"
+		);
+
+		//Import Datenbank
+
+		//Map
+		currentTrust.mySize = map(
+			currentTrust.myCountryArea,
+			17098250,
+			50,
+			700,
+			5
+		); // [17098250,50]
+
+		currentTrust.myColor = color(200, 100, 100);
+
+		myTrust[i] = currentTrust;
+		i++;
+	}
+}
+
+function draw() {
+	background(51);
+
+	let currentX = 10;
+	let currentY = 750;
+
+	//Display Trust
+	for (let i = 0; i < myTrust.length; i++) {
+		myTrust[i].display(currentX, currentY);
+		currentX += myTrust[i].myWidth + 2;
+	}
+
+	//FrameRate
+	fill(200);
+	textSize(12);
+	text("frameRate:   " + Math.round(frameRate()), 10, height - 5);
+}
